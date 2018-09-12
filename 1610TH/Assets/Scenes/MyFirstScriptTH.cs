861c490689e323a07af3241c623beae8e1317cbe
Taylor Hall <taylor.s.hall100@gmail.com>
2018-09-12 14:41:47 -0600

    Added scripts and Squid Spritesheet

---
+++ 1610TH/Assets/Scenes/MyFirstScriptTH.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScriptTH : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Awake ()
+	{
+		print("Hello World");
+	}
+}
