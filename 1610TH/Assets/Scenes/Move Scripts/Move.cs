984cf03ea3daf41818f1a65a274ba846bfa3a126
Taylor Hall <taylor.s.hall100@gmail.com>
2018-09-12 15:34:42 -0600

    Cube Movement

---
+++ 1610TH/Assets/Scenes/Move Scripts/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour 
+{
+
+	// Use this for initialization
+	//private void Update () {
+		
+	//}
+	public CharacterController Character;
+	
+	public float Speed;
+	public Vector3 MoveSpeed;
+	public Vector3 RotateSpeed;
+	
+	// Update is called once per frame
+	private void Update()
+	{
+		//Movement of object controlled by arrow keys
+		MoveSpeed.x = Speed*Input.GetAxis("Horizontal");
+		MoveSpeed *= Time.deltaTime;
+
+		Character.Move(MoveSpeed);
+	}
+}
