7ebe3cdf72586687c5e052da3ef115de3f93104b
Taylor Hall <taylor.s.hall100@gmail.com>
2018-09-17 15:49:41 -0600

    AddMoney Created

---
+++ 1610TH/Assets/Scenes/Move Scripts/AddMoney.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMoney : MonoBehaviour
+{
+
+	public int Money = 10;
+	
+	// Use this for initialization
+	public void AddMoreMoney (int money)
+	{
+		Money += money;
+		//Money = Money + money;
+	}
+}
---
+++ 1610TH/Assets/Scenes/Move Scripts/Variabes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variabes : MonoBehaviour
+{
+	public bool IsAWizard;
+	
+	// without a number attatched to value name, the value is automaticaly 0 
+	public int AmmoCount = 100;
+	public int InventoryCount;
+	// when using a decimal number, put an f at the end of number
+	public float Health = 1.0f;
+	public float Money;
+
+	public string PlayerName;
+	public string Team = "Wolves";
+
+	public Vector3 Location;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		print(PlayerName);
+		print("has");
+		print("$"+Money);
+
+		if (IsAWizard)
+		{
+			print("You're a Wizard, " + PlayerName + ".");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Location;
+	}
+}
