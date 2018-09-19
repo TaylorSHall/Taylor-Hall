31905b52b80af65ba8e6bcfae6ce738512cde96a
Taylor Hall <taylor.s.hall100@gmail.com>
2018-09-19 15:40:49 -0600

    Add Value

---
+++ 1610TH/Assets/Scenes/Move Scripts/AddValue.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class AddValue : ScriptableObject
+{
+    public FloatData ValueObj;
+    public FloatData MaxValue;
+    public FloatData MinValue;
+    
+    
+    public UnityEvent EventMin;
+    public UnityEvent EventMax;
+    
+    
+    public void AddValueToObj(FloatData data)
+    {
+        ValueObj.Value += data.Value;
+        
+        if (ValueObj.Value >= MaxValue.Value)
+        {
+            EventMax.Invoke();
+        }
+
+        if (ValueObj.Value <= MinValue.Value)
+        {
+            EventMin.Invoke();
+        }
+    }
+}
