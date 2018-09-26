bd70e5345df122c7b510071446a2a6c0e823dfdc
Taylor Hall <taylor.s.hall100@gmail.com>
2018-09-25 22:39:55 -0600

    Swag Data and Event
    Yer a wizard, Harry, that is, if yeh got enough swag!

--- 1610TH/Assets/Scenes/Move Scripts/Variabes.cs
+++ 1610TH/Assets/Scenes/Move Scripts/Variabes.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Variabes : MonoBehaviour
 {
 	public bool IsAWizard;
+	const int Swagcap = 100;
 	
-	// without a number attatched to value name, the value is automaticaly 0 
+	// without a number attached to value name, the value is automatically 0 
 	public int AmmoCount = 100;
 	public int InventoryCount;
+
+	public int Swag;
 	// when using a decimal number, put an f at the end of number
 	public float Health = 1.0f;
 	public float Money;
@@ -25,6 +29,12 @@ public class Variabes : MonoBehaviour
 		print("has");
 		print("$"+Money);
 
+		if (Swag >= Swagcap)
+		{
+			IsAWizard = true;
+		}
+		
+		
 		if (IsAWizard)
 		{
 			print("You're a Wizard, " + PlayerName + ".");
