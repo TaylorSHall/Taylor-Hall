9b665520f393e65342696dd4ff12776417b7a5e8
Taylor Hall <taylor.s.hall100@gmail.com>
2018-10-01 12:11:58 -0600

    AddValue Changes
    Subtraction, Multiplication, and Division statements added to AddValue Script.

--- 1610TH/Assets/Scripts/Move Scripts/AddValue.cs
+++ 1610TH/Assets/Scripts/Move Scripts/AddValue.cs
@@ -15,10 +15,58 @@ namespace Scenes.Move_Scripts
         public UnityEvent EventMax;
     
     
+        //ADD
         public void AddValueToObj(FloatData data)
         {
             ValueObj.Value += data.Value;
         
+            if (ValueObj.Value >= MaxValue.Value)
+            {
+                EventMax.Invoke();
+            }
+
+            if (ValueObj.Value <= MinValue.Value)
+            {
+                EventMin.Invoke();
+            }
+        }
+        //SUBTRACT
+        public void SubtractValueFromObj(FloatData data)
+        {
+            ValueObj.Value -= data.Value;
+            
+            if (ValueObj.Value >= MaxValue.Value)
+            {
+                EventMax.Invoke();
+            }
+
+            if (ValueObj.Value <= MinValue.Value)
+            {
+                EventMin.Invoke();
+            }
+        }
+        
+        //MULTIPLY
+        public void MultiplyValueWithObj(FloatData data)
+        {
+            ValueObj.Value *= data.Value;
+            
+            if (ValueObj.Value >= MaxValue.Value)
+            {
+                EventMax.Invoke();
+            }
+
+            if (ValueObj.Value <= MinValue.Value)
+            {
+                EventMin.Invoke();
+            }
+        }
+        
+        //DIVIDE
+        public void DivideValueByObj(FloatData data)
+        {
+            ValueObj.Value /= data.Value;
+            
             if (ValueObj.Value >= MaxValue.Value)
             {
                 EventMax.Invoke();
