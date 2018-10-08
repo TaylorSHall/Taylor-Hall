61e760dd35e5c59cb957258d5ebc00a7d5dffbb1
Taylor Hall <taylor.s.hall100@gmail.com>
2018-10-08 15:35:05 -0600

    Controls, bullets, Etc.

--- 1610TH/Assets/Scripts/CharacterMover.cs
+++ 1610TH/Assets/Scripts/CharacterMover.cs
@@ -9,6 +9,8 @@ public class CharacterMover : MonoBehaviour
 	
 	public float Gravity = 9.81f;
 	public float JumpSpeed = 3.0f;
+	public float MoveSpeed = 3.0f; //jumping speed is the same as moving speed
+	private Vector3 rotation;
 	private Vector3 position;
 	// Use this for initialization
 	void Start ()
@@ -21,6 +23,11 @@ public class CharacterMover : MonoBehaviour
 	{
 		if (Controller.isGrounded) //bool that is part of the character controller
 		{
+			position.Set(0, 0, MoveSpeed * Input.GetAxis("Vertical"));
+			rotation.Set(0, Input.GetAxis("Horizontal"), 0);
+			transform.Rotate(rotation);
+			position = transform.TransformDirection(position);
+			
 			if (Input.GetButton("Jump"))
 			{
 				position.y = JumpSpeed;
---
+++ 1610TH/Assets/Scripts/Prefabs/Instancing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instancing : MonoBehaviour
+{
+
+	public GameObject Instance;
+	
+	// Use this for initialization
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.X)) //x key is assigned to GetKeyDown Function in Input Method to fire
+		{
+			Instantiate(Instance, transform.position, transform.rotation);
+		}
+	}
+	
+	
+	
+}
