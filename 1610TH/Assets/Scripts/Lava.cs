3b24dee9bbe97a48f8a2ba9c599c7cd763f4d950
Taylor Hall <taylor.s.hall100@gmail.com>
2018-10-11 22:00:22 -0600

    Lava

---
+++ 1610TH/Assets/Scripts/Lava.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+
+public class Lava : MonoBehaviour
+{
+
+	
+	
+	// Update is called once per frame
+	public void OnCollisionEnter(Collision player)
+	{
+		SceneManager.LoadScene("Game Over");
+		
+	}
+}
