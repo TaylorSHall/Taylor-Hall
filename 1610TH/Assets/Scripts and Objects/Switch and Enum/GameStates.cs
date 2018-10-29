0857e5efcfef1d594473540da122cdde6c1843d5
Taylor Hall <taylor.s.hall100@gmail.com>
2018-10-29 15:04:52 -0600

    Switch and Enum
    Come up with two more Enum (weapons, powerups, etc.) and a few more switch statements this week.

---
+++ 1610TH/Assets/Scripts and Objects/Switch and Enum/GameStates.cs
@@ -0,0 +1,14 @@
+﻿public class GameStates 
+	{
+		public enum States //containing three different states
+		{
+			Starting,
+			Loading,
+			Playing,
+			Cutscene,
+			Dying,
+			Ending
+		}
+
+		public States State; //any one of the three states enumerated above
+	}
---
+++ 1610TH/Assets/Scripts and Objects/Switch and Enum/SwitchStatements.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchStatements : MonoBehaviour
+{
+	public GameStates.States GameState;
+	public UnityEvent Starting, Loading, Playing, Ending, Dying;
+	
+	// Update is called once per frame
+	void Update () //Command will loop either state because switch is in update
+	{
+		switch (GameState)
+		{
+			case GameStates.States.Starting:
+				print("We are starting the Game.");
+				break; //stop and get out of the switch block
+			
+			case GameStates.States.Loading:
+				print("We are loading the Game.");
+				break; //stop and get out of the switch block
+
+			case GameStates.States.Dying:
+				print("You are dead. Try the Game again.");
+				break; //stop and get out of the switch block
+			
+			case GameStates.States.Playing:
+				print("We are playing the Game.");
+				break; //stop and get out of the switch block
+			
+			case GameStates.States.Ending:
+				print("We are ending the Game. Game Over.");
+				break; //stop and get out of the switch block
+		}
+	}
+}
