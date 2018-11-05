a5aee0f46b70ab443db1616e2436c008e536c252
Taylor Hall <taylor.s.hall100@gmail.com>
2018-11-05 15:07:50 -0700

    Simple AI

--- 1610TH/Assets/Scripts and Objects/Switch and Enum/PowerupEnum.cs
+++ 1610TH/Assets/Scripts and Objects/Switch and Enum/PowerupEnum.cs
@@ -4,15 +4,34 @@ using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class PowerupEnum : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class PowerupEnum : MonoBehaviour
+{
+	public PowerupStates.PowerStates ShulkState;
+	public UnityEvent Null, Speed, Jump, Shield, Buster;
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		switch (ShulkState)
+		{
+			case PowerupStates.PowerStates.Null:
+				break;
+
+			case PowerupStates.PowerStates.Speed:
+				print("Speed on!");
+				break;
+
+			case PowerupStates.PowerStates.Jump:
+				print("Jump on!");
+				break;
+
+			case PowerupStates.PowerStates.Shield:
+				print("Shield on!");
+				break;
+
+			case PowerupStates.PowerStates.Buster:
+				print("Buster on!");
+				break;
+		}
 	}
 }
