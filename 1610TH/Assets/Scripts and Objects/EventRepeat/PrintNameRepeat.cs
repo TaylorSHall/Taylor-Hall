dcf3f073e3126ac4ddab767b5575d55f26623e9a
Taylor Hall <taylor.s.hall100@gmail.com>
2018-11-12 15:45:26 -0700

    Animation Lecture

---
+++ 1610TH/Assets/Scripts and Objects/EventRepeat/PrintNameRepeat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintNameRepeat : MonoBehaviour
+{
+
+	public string RepName;
+	// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		print("Some people call me " + RepName);
+	}
+}
