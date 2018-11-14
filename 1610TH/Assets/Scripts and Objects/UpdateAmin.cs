7f906de0a1440defb5723cfff136293a68776324
Taylor Hall <taylor.s.hall100@gmail.com>
2018-11-14 15:15:52 -0700

    Interactive Animation Lecture

---
+++ 1610TH/Assets/Scripts and Objects/UpdateAmin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class UpdateAmin : MonoBehaviour
+{
+
+
+	private Animator anims;
+
+	public float Speed;
+	// Use this for initialization
+	void Start ()
+	{
+		anims = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		anims.SetFloat("Speed", Speed);
+	}
+}
